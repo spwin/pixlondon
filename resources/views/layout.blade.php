320cec298bdd156a7878d69f048dad8ca955bf0e
Stanislav <stanislav@pixsens.co.uk>
2016-03-14 13:23:59 +0000

    added tawk.to script

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -23,6 +23,19 @@
     <script src="https://apis.google.com/js/platform.js" async defer>
         {lang: 'en-GB'}
     </script>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/56e6bae34d7862fe7275f9b9/default';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
     @yield('scripts')
 </body>
 </html>
