cafcc3af4101f1e002ab14167a268e35b089e91d
Stanislav <stanislav.markevic@gmail.com>
2016-03-15 14:39:07 -0400

    inserted analytics code

--- resources/lang/pl/layout.php
+++ resources/lang/pl/layout.php
@@ -2,5 +2,6 @@
 return [
     'description' => 'Nasze specjalizacje – tworzenie stron internetowych, dizajn www, SEO, tworzenie responsive stron, logo oraz znaków towarowych. Breathing life into pixels.',
     'keywords' => 'konstrukcja stron internetowych london, web agencja london, usługi SEO london, tworzenie stron internetowych london, reklama w internecie london, pixsens',
-    'tawk' => '56e6bfe51e82531d5694bc39'
+    'tawk' => '56e6bfe51e82531d5694bc39',
+    'analytics' => 'UA-68131288-3'
 ];
